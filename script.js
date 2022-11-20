57a03c8778fb89d01de3dd012eeb41aae19e7ad1
zion-off <ajz317@nyu.edu>
2022-11-20 20:51:15 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+function fadepanel1() {
+  $(".panel-1").fadeOut("3000");
+  setTimeout(() => {
+    $(".panel-2").fadeIn("3000");
+    $(".panel-1").css("visibility", "hidden");
+  }, 3000)
+  $(".panel-2").css("visibility", "visible");
+}
+
+function fadepanel2() {
+  $(".panel-2").fadeOut("3000");
+  setTimeout(() => {
+    $(".panel-3").fadeIn("3000");
+    $(".panel-2").css("visibility", "hidden");
+  }, 3000)
+  $(".panel-3").css("visibility", "visible");
+}
+
+function fadepanel3() {
+  $(".panel-3").fadeOut("3000");
+  setTimeout(() => {
+    $(".panel-4").fadeIn("3000");
+    $(".panel-3").css("visibility", "hidden");
+  }, 3000)
+  $(".panel-4").css("visibility", "visible");
+}
+
+function fadepanel4() {
+  $(".panel-4").fadeOut("3000");
+  setTimeout(() => {
+    $(".panel-5").fadeIn("3000");
+    $(".panel-4").css("visibility", "hidden");
+  }, 3000)
+  $(".panel-5").css("visibility", "visible");
+}
+
+
