c67f585db4bd535fe3e07a5d837da86fafc332db
zion-off <ajz317@nyu.edu>
2022-11-21 14:03:42 +0400

    final

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function fadepanel1() {
   $(".panel-1").fadeOut("3000");
   $(".panel-2").fadeIn("3000");
-  $(".panel-1").css("visibility", "hidden");
   $(".panel-2").css("visibility", "visible");
 }
 
-function fadepanel2(element) {
+function fadepanel2() {
   $(".panel-2").fadeOut("3000");
   $(".panel-3").fadeIn("3000");
-  $(".panel-2").css("visibility", "hidden");
   $(".panel-3").css("visibility", "visible");
   $('#step-1').get(0).stopVideo();
 }
@@ -16,7 +14,6 @@ function fadepanel2(element) {
 function fadepanel3() {
   $(".panel-3").fadeOut("3000");
   $(".panel-4").fadeIn("3000");
-  $(".panel-3").css("visibility", "hidden");
   $(".panel-4").css("visibility", "visible");
   $('#step-2').get(0).stopVideo();
 }
@@ -24,7 +21,6 @@ function fadepanel3() {
 function fadepanel4() {
   $(".panel-4").fadeOut("3000");
   $(".panel-5").fadeIn("3000");
-  $(".panel-4").css("visibility", "hidden");
   $(".panel-5").css("visibility", "visible");
   $('#step-3').get(0).stopVideo();
 }
